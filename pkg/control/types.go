dc209b718089a4dc20efce29edf16f1cecb44896
Luke Strickland <luke@axxim.net>
2023-02-25 14:31:01 -0500

    Stringer for channel & stream ID

--- pkg/control/types.go
+++ pkg/control/types.go
@@ -1,5 +1,15 @@
 package control
 
+import "fmt"
+
 type ChannelID uint32
 type StreamID uint32
 type StreamKey []byte
+
+func (id ChannelID) String() string {
+	return fmt.Sprintf("%d", id)
+}
+
+func (id StreamID) String() string {
+	return fmt.Sprintf("%d", id)
+}
