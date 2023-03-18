4661c339e0bdaaabf0368e68b56b09aaf907e16f
Luke Strickland <luke@axxim.net>
2023-03-18 14:07:33 -0400

    Fixing endpoint

--- pkg/service/glimesh/glimesh.go
+++ pkg/service/glimesh/glimesh.go
@@ -31,6 +31,7 @@ type Service struct {
 
 func New(endpoint, clientID, clientSecret string) *Service {
 	return &Service{
+		Endpoint: endpoint,
 		tokenUrl: "/api/oauth/token",
 		apiUrl:   "/api/graph",
 	}
